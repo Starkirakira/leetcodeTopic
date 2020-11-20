fd76bc9b8610d94dc29fcf02323315a1da9e05b0
Alice Kenn <kenncandy0618@gmail.com>
2020-11-20 10:53:04 +0800

    暂存修改

--- main.go
+++ main.go
@@ -1,70 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
 
-type Codec struct {
-	l []string
-}
-
-type TreeNode struct {
-	Val int
-	Left *TreeNode
-	Right *TreeNode
-}
 
 func main()  {
-	//a := []string{"1","2","3","null","null","4","5"}
-	b := Constructor()
+	//***二叉树序列化与反序列化***//
+	//b := Constructor()
+	//res := b.deserialize("1,2,3,null,null,4,null,null,5,null,null")
+	//fmt.Printf("%+v",res)
+	//
 
-	res := b.deserialize("1,2,3,null,null,4,null,null,5,null,null")
-	fmt.Printf("%+v",res)
+	//***水域大小 1838***//
+
 }
 
-func Constructor() Codec {
-	return Codec{}
-}
-
-func (this *Codec) serialize(root *TreeNode) string  {
-	return rserialize(root,"")
-}
-
-func rserialize(root *TreeNode, str string) string {
-	if root == nil{
-		str += "null,"
-	}else{
-		str += strconv.Itoa(root.Val) + ","
-		str = rserialize(root.Left, str)
-		str = rserialize(root.Right, str)
-	}
-	return str
-}
-
-func (this *Codec) deserialize(data string)	*TreeNode  {
-	l := strings.Split(data, ",")
-	for i :=0; i < len(l); i++ {
-		if l[i] != ""{
-			this.l = append(this.l , l[i])
-		}
-	}
-	//fmt.Println(this.l)
-	return this.rdeserialize()
-}
-
-func (this *Codec) rdeserialize() *TreeNode {
-	if this.l[0] == "null"{
-		this.l = this.l[1:]
-		return nil
-	}
-
-	val,_ := strconv.Atoi(this.l[0])
-	root := &TreeNode{Val: val}
-	this.l = this.l[1:]
-	root.Left = this.rdeserialize()
-	root.Right = this.rdeserialize()
-	return root
-}
 
---
+++ pondSizes.go
@@ -0,0 +1,11 @@
+package main
+
+func pondSizes(land [][]int) []int{
+	for i := 0; i < len(land); i++ {
+		for j:=0; j<len(land[i]);j++{
+
+		}
+	}
+
+	return []int{1}
+}

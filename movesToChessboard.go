5953b996f3cf04fc7a472fdedc74f8e32e4bb301
Starkirakira <1179284845@qq.com>
2020-11-30 23:59:24 +0800

    111

--- movesToChessboard.go
+++ movesToChessboard.go
@@ -1,5 +1,21 @@
 package main
 
+import "os"
+
 func movesToChessboard(board [][]int) int {
+	N := len(board)
+	count := make(map[int]int)
+
+	for _,row:= range board{
+		code := 0
+		for x:= range row{
+			code = 2 * code + x
+		}
+		if _,ok:= count[code];!ok{
+			
+		}
+	}
+
+
 	return 0
 }

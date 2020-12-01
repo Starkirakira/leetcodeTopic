d38e1668241def05f5d5872be38aee40d8495fed
Starkirakira <1179284845@qq.com>
2020-12-01 23:52:52 +0800

    1743.数据流中的中位数

--- medianFinder.go
+++ medianFinder.go
@@ -1,18 +1,21 @@
 package main
 
 type MedianFinder struct {
-
+	listA []int //保存前半部分的较小值
+	listB []int//保存后半部分的较大值
 }
 
 
 /** initialize your data structure here. */
 func ConstructorMedian() MedianFinder {
-
+	return MedianFinder{listA: []int{}, listB: []int{}}
 }
 
 
 func (this *MedianFinder) AddNum(num int)  {
-
+	if len(this.listA) != len(this.listB) {
+		
+	}
 }
 
 

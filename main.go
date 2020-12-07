ec7948545794e63eea2c227507a708c5198b4701
Alice Kenn <kenncandy0618@gmail.com>
2020-12-07 16:04:47 +0800

    449.序列化和反序列化二叉搜索树

--- main.go
+++ main.go
@@ -120,9 +120,23 @@ func main()  {
 	///***
 	//377.给定一个由正整数组成且不存在重复数字的数组，找出和为给定目标正整数的组合的个数。
 	//***///
-	nums := []int{1,2,3}
-	target := 4
-	fmt.Println(combinationSum4(nums, target))
+	//nums := []int{1,2,3}
+	//target := 4
+	//fmt.Println(combinationSum4(nums, target))
+
+	//***
+	//序列化是将数据结构或对象转换为一系列位的过程，以便它可以存储在文件或内存缓冲区中，或通过网络连接链路传输，以便稍后在同一个或另一个计算机环境中重建。
+	//设计一个算法来序列化和反序列化 二叉搜索树 。 对序列化/反序列化算法的工作方式没有限制。 您只需确保二叉搜索树可以序列化为字符串，并且可以将该字符串反序列化为最初的二叉搜索树。
+	//编码的字符串应尽可能紧凑。
+	//***//
+
+	root := TreeNodeii{1,&TreeNodeii{2,nil,nil},&TreeNodeii{3,nil,nil}}
+	ser := Constructorii()
+	deser := Constructorii()
+	tree := ser.serializeii(&root)
+	ans := deser.deserializeii(tree)
+	fmt.Println(ans)
+
 
 
 

8d075a565108192b74fe9bd616ede3d8aaa94d53
Alice Kenn <kenncandy0618@gmail.com>
2020-12-15 17:17:30 +0800

    677.前缀和字典树

--- src/main.go
+++ src/main.go
@@ -236,8 +236,26 @@ func main()  {
 	//给你一个整数数组 arr。你可以从中选出一个整数集合，并删除这些整数在数组中的每次出现。
 	//返回 至少 能删除数组中的一半整数的整数集合的最小大小。
 	//***//
-	arr := []int{3,3,3,3,5,5,5,2,2,7}
-	fmt.Println(lc.MinSetSize(arr))
+	//arr := []int{3,3,3,3,5,5,5,2,2,7}
+	//fmt.Println(lc.MinSetSize(arr))
+
+	//***
+	//实现一个 MapSum 类，支持两个方法，insert和sum：
+	//MapSum() 初始化 MapSum 对象
+	//void insert(String key, int val) 插入 key-val 键值对，字符串表示键 key ，整数表示值 val 。如果键 key 已经存在，那么原来的键值对将被替代成新的键值对。
+	//int sum(string prefix) 返回所有以该前缀 prefix 开头的键 key 的值的总和。
+	//链接：https://leetcode-cn.com/problems/map-sum-pairs
+	//***//
+	obj := lc.ConstructorMapSum()
+	obj.Insert("apple", 3)
+	param_2 := obj.Sum("ap")
+	fmt.Println(param_2)
+	obj.Insert("app", 2)
+	param_3 := obj.Sum("ap")
+	fmt.Println(param_3)
+
+
+
 
 
 

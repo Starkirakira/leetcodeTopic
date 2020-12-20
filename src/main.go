8d6dccded46cec7a56631d646d4c617d35b55a6b
Starkirakira <1179284845@qq.com>
2020-12-20 22:25:34 +0800

    316.最低字典序去重

--- src/main.go
+++ src/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"leetcode/lc"
+)
+
 func main()  {
 
 	///***二叉树的序列化与反序列化***///
@@ -342,6 +347,11 @@ func main()  {
 	//	}}
 	//fmt.Println(lc.CopyRandomList(head))
 
+	//***
+	//给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+	//***//
+	s := "bcabc"
+	fmt.Println(lc.RemoveDuplicateLetters(s))
 
 
 

e01b825c63c0d6e1447673739d26dd1a78c574af
Alice Kenn <kenncandy0618@gmail.com>
2020-12-29 14:58:01 +0800

    330.按要求补齐数组

---
+++ src/lc/minPatches.go
@@ -0,0 +1,16 @@
+package lc
+
+func MinPatches(nums []int, n int) int {
+	ans := 0
+	for i,x := 0,1; x <= n; {
+		if i < len(nums) && nums[i] <= x {
+			x += nums[i]
+			i++
+		} else {
+			x *= 2
+			ans++
+		}
+	}
+	return ans
+
+}

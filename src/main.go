80f28802fef462576833644fdbaa309eea755d42
Starkirakira <1179284845@qq.com>
2021-01-16 23:58:33 +0800

    803.打砖块，并查集

--- src/main.go
+++ src/main.go
@@ -798,13 +798,25 @@ func main()  {
 	//返回使每个结点上只有一枚硬币所需的移动次数。
 	//链接：https://leetcode-cn.com/problems/distribute-coins-in-binary-tree
 	//***//
-	root := &lc.TreeNode{
-		3,
-		&lc.TreeNode{0,nil,nil,},
-		&lc.TreeNode{0,nil,nil,},
-	}
-	fmt.Println(lc.DistributeCoins(root))
+	//root := &lc.TreeNode{
+	//	3,
+	//	&lc.TreeNode{0,nil,nil,},
+	//	&lc.TreeNode{0,nil,nil,},
+	//}
+	//fmt.Println(lc.DistributeCoins(root))
 
+	//***
+	//有一个 m x n 的二元网格，其中 1 表示砖块，0 表示空白。砖块 稳定（不会掉落）的前提是：
+	//一块砖直接连接到网格的顶部，或者
+	//至少有一块相邻（4个方向之一）砖块 稳定 不会掉落时
+	//给你一个数组 hits ，这是需要依次消除砖块的位置。每当消除hits[i] = (rowi, coli) 位置上的砖块时，对应位置的砖块（若存在）会消失，然后其他的砖块可能因为这一消除操作而掉落。一旦砖块掉落，它会立即从网格中消失（即，它不会落在其他稳定的砖块上）。
+	//返回一个数组 result ，其中 result[i] 表示第 i 次消除操作对应掉落的砖块数目。
+	//注意，消除可能指向是没有砖块的空白位置，如果发生这种情况，则没有砖块掉落。
+	//链接：https://leetcode-cn.com/problems/bricks-falling-when-hit
+	//***//
+	grid := [][]int{{1,0,0,0},{1,1,1,0}}
+	hits := [][]int{{1,0}}
+	fmt.Println(lc.HitBricks(grid, hits))
 
 
 

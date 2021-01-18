b2038290d092404f316edd55ea89a2e3335db76f
Starkirakira <1179284845@qq.com>
2021-01-18 22:03:58 +0800

    721.账户合并

--- src/main.go
+++ src/main.go
@@ -831,8 +831,17 @@ func main()  {
 	//请你来判断，这些点是否在该坐标系中属于同一条直线上，是则返回 true，否则请返回 false。
 	//链接：https://leetcode-cn.com/problems/check-if-it-is-a-straight-line
 	//***//
-	coordinates := [][]int{{1,2},{2,3},{3,4},{4,5},{5,6}}
-	fmt.Println(lc.CheckStraightLine(coordinates))
+	//coordinates := [][]int{{1,2},{2,3},{3,4},{4,5},{5,6}}
+	//fmt.Println(lc.CheckStraightLine(coordinates))
+
+	//***
+	//给定一个列表 accounts，每个元素 accounts[i]是一个字符串列表，其中第一个元素 accounts[i][0]是名称 (name)，其余元素是 emails 表示该账户的邮箱地址。
+	//现在，我们想合并这些账户。如果两个账户都有一些共同的邮箱地址，则两个账户必定属于同一个人。请注意，即使两个账户具有相同的名称，它们也可能属于不同的人，因为人们可能具有相同的名称。一个人最初可以拥有任意数量的账户，但其所有账户都具有相同的名称。
+	//合并账户后，按以下格式返回账户：每个账户的第一个元素是名称，其余元素是按字符 ASCII 顺序排列的邮箱地址。账户本身可以以任意顺序返回。
+	//链接：https://leetcode-cn.com/problems/accounts-merge
+	//***//
+	accounts := [][]string{{"John", "johnsmith@mail.com", "john00@mail.com"}, {"John", "johnnybravo@mail.com"}, {"John", "johnsmith@mail.com", "john_newyork@mail.com"}, {"Mary", "mary@mail.com"}}
+	fmt.Println(lc.AccountsMerge(accounts))
 
 
 

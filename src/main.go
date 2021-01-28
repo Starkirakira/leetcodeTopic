a9083da39ace2cee6ca4b7b4e5e2b0e02d0cc8ee
Starkirakira <1179284845@qq.com>
2021-01-28 21:25:07 +0800

    724.寻找中心索引

--- src/main.go
+++ src/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"leetcode/myLab"
+	"leetcode/lc"
 )
 
 func main()  {
@@ -937,11 +937,19 @@ func main()  {
 	//channel接收阻塞测试
 	//***//
 	//done := make(chan bool)
-	go myLab.AGoroutine()
- 
-	myLab.Done <- true
-	fmt.Println(myLab.Msg)
+	//go myLab.AGoroutine()
+	//myLab.Done <- true
+	//fmt.Println(myLab.Msg)
 
+	//***
+	//给你一个整数数组nums，请编写一个能够返回数组 “中心索引” 的方法。
+	//数组 中心索引 是数组的一个索引，其左侧所有元素相加的和等于右侧所有元素相加的和。
+	//如果数组不存在中心索引，返回 -1 。如果数组有多个中心索引，应该返回最靠近左边的那一个。
+	//注意：中心索引可能出现在数组的两端。
+	//链接：https://leetcode-cn.com/problems/find-pivot-index
+	//***//
+	nums := []int{1,7,3,6,5,6}
+	fmt.Println(lc.PivotIndex(nums))
 
 
 

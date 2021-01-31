2ca21edcc9a6ebb8a0f496233412f6f30aeb2138
Starkirakira <1179284845@qq.com>
2021-01-31 23:10:29 +0800

    更新并查集模型

--- src/lc/findCriticalAndPseudoCriticalEdges.go
+++ src/lc/findCriticalAndPseudoCriticalEdges.go
@@ -5,41 +5,6 @@ import (
 	"sort"
 )
 
-type unionFind1 struct {
-	parent, size []int
-	setCount int
-}
-
-func newUnionFind1(n int) *unionFind1 {
-	parent := make([]int, n)
-	size := make([]int, n)
-	for i := range parent {
-		parent[i] = i
-		size[i] = 1
-	}
-	return &unionFind1{parent, size, n}
-}
-
-func (uf *unionFind1) find(x int) int {
-	if uf.parent[x] != x {
-		uf.parent[x] = uf.find(uf.parent[x])
-	}
-	return uf.parent[x]
-}
-
-func (uf *unionFind1) union(x,y int) bool {
-	fx, fy := uf.find(x), uf.find(y)
-	if fx == fy {
-		return false
-	}
-	if uf.size[fx] < uf.size[fy] {
-		fx, fy = fy, fx
-	}
-	uf.size[fx] += uf.size[fy]
-	uf.parent[fy] = fx
-	uf.setCount--
-	return true
-}
 
 func FindCriticalAndPseudoCriticalEdges(n int, edges [][]int) [][]int {
 	for i, e := range edges {

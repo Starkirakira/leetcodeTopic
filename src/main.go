76ddce03b5dc52c0f01658cf7b7fda34277f3a51
Starkirakira <1179284845@qq.com>
2021-02-22 21:27:46 +0800

    1438.绝对查不超过限制的最长子数组

--- src/main.go
+++ src/main.go
@@ -1158,7 +1158,16 @@ func main()  {
 	//在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
 	//链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
 	//***//
-	fmt.Println(lc.FindRepeatNumbe([]int{2,3,1,0,2,5,3}))
+	//fmt.Println(lc.FindRepeatNumbe([]int{2,3,1,0,2,5,3}))
+
+	//***
+	//给你一个整数数组 nums ，和一个表示限制的整数 limit，请你返回最长连续子数组的长度，该子数组中的任意两个元素之间的绝对差必须小于或者等于 limit 。
+	//如果不存在满足条件的子数组，则返回 0 。
+	//链接：https://leetcode-cn.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit
+	//***//
+	nums := []int{8,2,4,7}
+	limit := 4
+	fmt.Println(lc.LongestSubarray(nums,limit))
 
 
 

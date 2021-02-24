c990012c3a2e698433106d43b1dac905c4251934
Starkirakira <1179284845@qq.com>
2021-02-24 22:34:13 +0800

    832.翻转反转矩阵

--- src/main.go
+++ src/main.go
@@ -1176,10 +1176,19 @@ func main()  {
 	//请你返回这一天营业下来，最多有多少客户能够感到满意的数量。
 	//链接：https://leetcode-cn.com/problems/grumpy-bookstore-owner
 	//***//
-	customers := []int{1,0,1,2,1,1,7,5}
-	grumpy := []int{0,1,0,1,0,1,0,1}
-	x := 3
-	fmt.Println(lc.MaxSatisfied(customers, grumpy, x))
+	//customers := []int{1,0,1,2,1,1,7,5}
+	//grumpy := []int{0,1,0,1,0,1,0,1}
+	//x := 3
+	//fmt.Println(lc.MaxSatisfied(customers, grumpy, x))
+
+	//***
+	//给定一个二进制矩阵A，我们想先水平翻转图像，然后反转图像并返回结果。
+	//水平翻转图片就是将图片的每一行都进行翻转，即逆序。例如，水平翻转[1, 1, 0]的结果是[0, 1, 1]。
+	//反转图片的意思是图片中的0全部被1替换，1全部被0替换。例如，反转[0, 1, 1]的结果是[1, 0, 0]。
+	//链接：https://leetcode-cn.com/problems/flipping-an-image
+	//***//
+	A := [][]int{{1,1,0},{1,0,1},{0,0,0}}
+	fmt.Println(lc.FlipAndInvertImage(A))
 
 
 

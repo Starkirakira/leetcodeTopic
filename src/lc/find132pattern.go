fe05806bd19a17f5e4682f9dcdf7276e599dc660
Starkirakira <1179284845@qq.com>
2021-03-24 20:47:47 +0800

    456.找到是否存在132模式的子数组

---
+++ src/lc/find132pattern.go
@@ -0,0 +1,140 @@
+package lc
+
+import (
+	find123 "math/rand"
+)
+
+type node struct {
+	ch [2]*node
+	priority int
+	val int
+	cnt int
+}
+
+func (o *node) cmp(b int) int {
+	switch  {
+	case b < o.val:
+		return 0
+	case b > o.val:
+		return 1
+	default:
+		return -1
+	}
+}
+
+func (o *node) rotate(d int) *node {
+	x := o.ch[d^1]
+	o.ch[d^1] = x.ch[d]
+	x.ch[d] = o
+	return x
+}
+
+type treap struct {
+	root *node
+}
+
+func (t *treap) _put(o *node, val int) *node {
+	if o == nil {
+		return &node{priority: find123.Int(), val: val, cnt: 1}
+	}
+	if d := o.cmp(val); d >= 0 {
+		o.ch[d] = t._put(o.ch[d], val)
+		if o.ch[d].priority > o.priority {
+			o = o.rotate(d^1)
+		}
+	} else {
+		o.cnt++
+	}
+	return o
+}
+
+func (t *treap) put(val int) {
+	t.root = t._put(t.root, val)
+}
+
+func (t *treap) _delete(o *node, val int) *node {
+	if o == nil {
+		return nil
+	}
+	if d := o.cmp(val); d >= 0{
+		o.ch[d] = t._delete(o.ch[d], val)
+		return o
+	}
+	if o.cnt > 1 {
+		o.cnt--
+		return o
+	}
+	if o.ch[1] == nil {
+		return o.ch[0]
+	}
+	if o.ch[0] == nil {
+		return o.ch[1]
+	}
+	d := 0
+	if o.ch[0].priority > o.ch[1].priority {
+		d = 1
+	}
+	o = o.rotate(d)
+	o.ch[d] = t._delete(o.ch[d], val)
+	return o
+}
+
+func (t *treap) delete(val int)  {
+	t.root = t._delete(t.root, val)
+}
+
+func (t *treap) upperBound(val int) (ub *node) {
+	for o := t.root; o != nil; {
+		if o.cmp(val) == 0 {
+			ub = o
+			o = o.ch[1]
+		}
+	}
+	return
+}
+
+func Find132pattern(nums []int) bool {
+	n := len(nums)
+	if n < 3 {
+		return false
+	}
+	leftMin := nums[0]
+	rights := &treap{}
+	for _, v := range nums[2:] {
+		rights.put(v)
+	}
+
+	for j := 1; j < n - 1 ; j++ {
+		if nums[j] > leftMin {
+			ub := rights.upperBound(leftMin)
+			if ub != nil && ub.val < nums[j] {
+				return true
+			}
+		} else {
+			leftMin = nums[j]
+		}
+		rights.delete(nums[j + 1])
+	}
+	return false
+
+	//another way
+	//n := len(nums)
+	//candidateK := []int{nums[n-1]}
+	//maxK := math.MinInt64
+	//
+	//for i := n - 2; i >= 0; i-- {
+	//	if nums[i] < maxK {
+	//		return true
+	//	}
+	//	for len(candidateK) > 0 && nums[i] > candidateK[len(candidateK)-1] {
+	//		maxK = candidateK[len(candidateK)-1]
+	//		candidateK = candidateK[:len(candidateK)-1]
+	//	}
+	//	if nums[i] > maxK {
+	//		candidateK = append(candidateK, nums[i])
+	//	}
+	//}
+	//
+	//return false
+
+}
--- src/main.go
+++ src/main.go
@@ -1319,9 +1319,18 @@ func main()  {
 	//题目数据保证答案符合 32 位带符号整数范围。
 	//链接：https://leetcode-cn.com/problems/distinct-subsequences
 	//***//
-	s := "rabbbit"
-	t := "rabbit"
-	fmt.Println(lc.NumDistinct(s, t))
+	//s := "rabbbit"
+	//t := "rabbit"
+	//fmt.Println(lc.NumDistinct(s, t))
+
+	//***
+	//给你一个整数数组 nums ，数组中共有 n 个整数。132 模式的子序列 由三个整数 nums[i]、nums[j] 和 nums[k] 组成，并同时满足：i < j < k 和 nums[i] < nums[k] < nums[j] 。
+	//如果 nums 中存在 132 模式的子序列 ，返回 true ；否则，返回 false 。
+	//进阶：很容易想到时间复杂度为 O(n^2) 的解决方案，你可以设计一个时间复杂度为 O(n logn) 或 O(n) 的解决方案吗？
+	//链接：https://leetcode-cn.com/problems/132-pattern
+	//***//
+	nums := []int{1,2,3,4}
+	fmt.Println(lc.Find132pattern(nums))
 
 
 

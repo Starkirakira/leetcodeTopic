e146aae9219bc42e298abe716a8b77990d630b3f
Alice Kenn <kenncandy0618@gmail.com>
2021-08-13 17:01:32 +0800

    120.三角形最小路径和 动态规划

--- src/main.go
+++ src/main.go
@@ -1358,8 +1358,16 @@ func main() {
 	//你可以按任意顺序返回答案。
 	//链接：https://leetcode-cn.com/problems/two-sum
 	//***//
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	fmt.Println(lc.TwoSum(nums, target))
+	//nums := []int{2, 7, 11, 15}
+	//target := 9
+	//fmt.Println(lc.TwoSum(nums, target))
+
+	//***
+	//给定一个三角形 triangle ，找出自顶向下的最小路径和。
+	//每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+	//链接：https://leetcode-cn.com/problems/triangle
+	//***//
+	triangle := [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
+	fmt.Println(lc.MinimumTotal(triangle))
 
 }

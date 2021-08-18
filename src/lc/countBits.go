c500f6d90b78d7decaa0107d7af0c2eebb1c07f3
Alice Kenn <kenncandy0618@gmail.com>
2021-08-18 17:21:32 +0800

    2088.剑指offer03 改正答案：前N位二进制数里1的数量

--- src/lc/countBits.go
+++ src/lc/countBits.go
@@ -2,12 +2,8 @@ package lc
 
 func CountBits(num int) []int {
 	bits := make([]int, num+1)
-	hightBit := 0
 	for i := 1; i <= num; i++ {
-		if i & (i - 1) == 0 {
-			hightBit = 1
-		}
-		bits[i] = bits[i - hightBit] + 1
+		bits[i] = bits[i&(i-1)] + 1
 	}
 	return bits
 }

cc0006e0bd55cab4190841d09be57df4a197d686
Alice Kenn <kenncandy0618@gmail.com>
2021-08-19 17:27:28 +0800

    2094.剑指offer06 排序数组中两个数字之和

--- src/main.go
+++ src/main.go
@@ -1404,7 +1404,17 @@ func main() {
 	//给定一个字符串数组words，请计算当两个字符串 words[i] 和 words[j] 不包含相同字符时，它们长度的乘积的最大值。假设字符串中只包含英语的小写字母。如果没有不包含相同字符的一对字符串，返回 0。
 	//链接：https://leetcode-cn.com/problems/aseY1I
 	//***//
-	words := []string{"abcw", "baz", "foo", "bar", "fxyz", "abcdef"}
-	fmt.Println(lc.MaxProduct(words))
+	//words := []string{"abcw", "baz", "foo", "bar", "fxyz", "abcdef"}
+	//fmt.Println(lc.MaxProduct(words))
+
+	//***
+	//剑指offer 06 排序数组中两个数字之和
+	//给定一个已按照 升序排列 的整数数组numbers ，请你从数组中找出两个数满足相加之和等于目标数target 。
+	//函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 0开始计数 ，所以答案数组应当满足 0<= answer[0] < answer[1] <numbers.length。
+	//假设数组中存在且只存在一对符合条件的数字，同时一个数字不能使用两次。
+	//链接：https://leetcode-cn.com/problems/kLl5u1
+	//***//
+	numbers, target := []int{1, 2, 4, 6, 10}, 8
+	fmt.Println(lc.TwoNumberSum(numbers, target))
 
 }

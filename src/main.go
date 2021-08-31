5f490ed9d379cfc4cbad43ef2eb36e5c1d69f465
Starkirakira <1179284845@qq.com>
2021-08-31 21:52:04 +0800

    leetcode 剑指offerii 2３ 寻找链表相交节点 212７

--- src/main.go
+++ src/main.go
@@ -1554,6 +1554,14 @@ func main() {
 	//说明：不允许修改给定的链表。
 	//链接：https://leetcode-cn.com/problems/c32eOV
 	//***//
-	head := &lc.ListNode{3, &lc.ListNode{3, &lc.ListNode{3, &lc.ListNode{3, nil}}}}
-	fmt.Println(lc.DetectCycle(head))
+	//head := &lc.ListNode{3, &lc.ListNode{3, &lc.ListNode{3, &lc.ListNode{3, nil}}}}
+	//fmt.Println(lc.DetectCycle(head))
+
+	//***
+	//剑指 Offer II 023. 两个链表的第一个重合节点
+	//给定两个单链表的头节点 headA 和 headB ，请找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+	//***//
+	A := &lc.ListNode{4, &lc.ListNode{1, &lc.ListNode{8, &lc.ListNode{4, &lc.ListNode{5, nil}}}}}
+	B := &lc.ListNode{5, &lc.ListNode{0, &lc.ListNode{1, &lc.ListNode{8, &lc.ListNode{4, &lc.ListNode{5, nil}}}}}}
+	fmt.Println(lc.GetIntersectionNode(A, B))
 }
